badc2e71efb76fc514bd8f0e61814fdb90668788
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2015-01-04 16:11:48 +0100

    Reduce test verbosity

--- test/test1.sh
+++ test/test1.sh
@@ -1,44 +1,49 @@
 #!/usr/bin/env sh 
 set -e
 
+silent=true
+
+run() {
+  if [[ $silent == false ]] ; then
+    eval "$1 "
+	else 
+	eval "$1 &> /dev/null"
+  fi
+}
 
 bold=$(tput bold)
 reset=$(tput sgr0)
 
 function print_important_message {
-	echo ""
-	printf "${bold}$1${reset}. "
-	echo ""
+	if [[ $silent == false ]]; then
+		echo ""
+		printf "${bold}$1${reset}. "
+		echo ""
+	fi
+
 }
 
 srcdir=`dirname $0`
 srcdir=`cd $srcdir; pwd`
 
-pushd ${srcdir}/test1
+run "pushd ${srcdir}/test1"
 
 print_important_message "Removing previous test data"
-rm -rf _site 
-rm -rf .build
-rm -f ../actual.tree
+run "rm -rf _site"
+run "rm -rf .build"
+run "rm -f ../actual.tree"
 
 
 print_important_message "Creating makefile"
-coffee nmake.coffee
+run "coffee nmake.coffee"
 
 
 print_important_message "Running test"
-make all
+run "make all"
 tree > ../actual.tree
-diff ../actual.tree final.tree
+diff-files ../actual.tree final.tree -m "Test base 0 - Exec"
 
-if [ $? -eq 0 ]
-then
-   print_important_message "TEST OK"
-else
-   print_important_message "TEST FAILED!!!!"
-fi
-
-rm -f ../actual.tree
+run "rm -f ../actual.tree"
 
 
-popd
+run "popd"
--- test/test2.sh
+++ test/test2.sh
@@ -1,54 +1,57 @@
 #!/usr/bin/env sh 
 set -e
 
+silent=true
+
+run() {
+  if [[ $silent == false ]] ; then
+    eval "$1 "
+	else 
+	eval "$1 &> /dev/null"
+  fi
+}
 
 bold=$(tput bold)
 reset=$(tput sgr0)
 
 function print_important_message {
-	echo ""
-	printf "${bold}$1${reset}. "
-	echo ""
+	if [[ $silent == false ]]; then
+		echo ""
+		printf "${bold}$1${reset}. "
+		echo ""
+	fi
+
 }
 
 srcdir=`dirname $0`
 srcdir=`cd $srcdir; pwd`
 
-pushd ${srcdir}/test2
+run "pushd ${srcdir}/test2"
 
 print_important_message "Removing previous test data"
-rm -rf _site 
-rm -rf .build
-rm -f ../actual.tree
-
+run "rm -rf _site"
+run "rm -rf .build"
+run "rm -f ../actual.tree"
 
 print_important_message "Creating makefile"
-coffee nmake.coffee
+run "coffee nmake.coffee"
 
 print_important_message "Running pre-test"
-make all
+run "make all"
 
 print_important_message "Cleaning up"
-make clean
+run "make clean"
 
 print_important_message "Checking if cleaned"
 tree -a > ../actual.tree
-diff ../actual.tree orig.tree
+diff-files ../actual.tree orig.tree  -m "Test base 1 - Cleaned"
 
 print_important_message "Building for final check"
-make all
+run "make all"
 
 tree -a > ../actual.tree
-diff ../actual.tree final.tree
+diff-files ../actual.tree final.tree -m "Test base 1 - Exec"
 
-if [ $? -eq 0 ]
-then
-   print_important_message "TEST OK"
-else
-   print_important_message "TEST FAILED!!!!"
-fi
+run "rm -f ../actual.tree"
 
-rm -f ../actual.tree
-
-
-popd
+run "popd"
